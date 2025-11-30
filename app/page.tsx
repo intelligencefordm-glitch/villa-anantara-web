762e5d33d2e6bddce9d644c9b62d0ee3a360bbc9
intelligencefordm-glitch <intelligencefordm@gmail.com>
2025-12-01 01:56:34 +0530

    Fix allowFullScreen attribute in iframe

--- app/page.tsx
+++ app/page.tsx
@@ -153,7 +153,7 @@ export default function Home() {
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3688.324276774168!2d81.65166437530348!3d21.255514479998443!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a28ddd262d23f09%3A0xa990e58aae55ee4f!2sVilla%20Anantara!5e0!3m2!1sen!2sin!4v1701101200000!5m2!1sen!2sin"
             width="100%"
             height="250"
-            allowFullScreen=""
+            allowFullScreen
             loading="lazy"
             referrerPolicy="no-referrer-when-downgrade"
           ></iframe>
