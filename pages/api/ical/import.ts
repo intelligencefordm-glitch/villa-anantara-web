640f8bc0229dd6dc0751a97b69286eae07932b75
intelligencefordm-glitch <intelligencefordm@gmail.com>
2025-12-02 01:11:15 +0530

    new changes

--- pages/api/ical/import.ts
+++
@@ -1,5 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-// This endpoint is a stub. In production run a scheduled job to fetch external iCals (Airbnb/Booking.com)
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ ok: true, note: 'Import scheduled job should run server-side to fetch iCal URLs stored in settings.' });
-}
